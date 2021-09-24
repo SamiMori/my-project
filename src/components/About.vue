1d291c03407b0e2c07eb8f18a0d75b0ebd1f3b6b
SamiMori <moriwakisami@gmail.com>
2021-09-24 08:11:05 -0300

    v1.0.0

---
+++ src/components/About.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <div>
+      <input type="text" v-model="text">
+      <button @click="save">Save</button>
+    </div>
+    <p v-for="(t, index) in saveText" :key="index"><strong>{{t.text}}</strong> {{t.date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'Welcome to Your About Page!!',
+      text: '',
+      saveText: []
+    }
+  },
+  methods: {
+    save () {
+      var date = new Date()
+      var minute = date.getMinutes()
+      var hour = date.getHours()
+      this.saveText.push({text: this.text, date: `${hour}:${minute}`})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
